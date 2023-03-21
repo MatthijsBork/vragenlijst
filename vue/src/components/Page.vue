c6d6c9826daf611f99e3bc5191337c9bbe861467
unknown <mborkhuis@st.noorderpoort.nl>
2023-03-21 11:04:26 +0100

    survey cards

--- vue/src/components/Page.vue
+++ vue/src/components/Page.vue
@@ -2,13 +2,14 @@
   <header class="bg-white shadow">
     <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ title }}</h1>
+      <slot name="header"></slot>
     </div>
   </header>
   <main>
-    <div class="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="bg-light py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <!-- Replace with your content -->
       <div class="px-4 py-6 sm:px-0">
-        <slot name="header"></slot>
+        <slot></slot>
       </div>
       <!-- /End replace -->
     </div>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -21,7 +21,6 @@ const routes = [
       { path: '/surveys', name: 'Surveys', component: Surveys },
       { path: '/surveys/create', name: 'SurveyCreate', component: SurveyView },
       { path: '/surveys/:id', name: 'SurveyView', component: SurveyView },
-
     ]
   },
   {
